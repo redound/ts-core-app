7d44d583f01d9bf25dabc8cc53a9498146c68e83
Olivier Andriessen <olivierandriessen@gmail.com>
2015-11-16 22:14:08 +0100

    Add test stub

--- test/TSCore/TSCore.spec.ts
+++ test/TSCore/TSCore.spec.ts
@@ -1,1 +1,11 @@
 /// <reference path="../../build/ts-core-app.d.ts" />
+
+declare var describe, it, expect, jasmine, beforeEach;
+
+describe("TSCore.App", function() {
+
+	it("test should work", () => {
+
+		expect(true).toEqual(true);
+	});
+});
--- test/ts-core-app.spec.js
+++ test/ts-core-app.spec.js
@@ -1,1 +1,6 @@
-/// <reference path="../../build/ts-core-app.d.ts" /> 
+/// <reference path="../../build/ts-core-app.d.ts" />
+describe("TSCore.App", function () {
+    it("test should work", function () {
+        expect(true).toEqual(true);
+    });
+});
